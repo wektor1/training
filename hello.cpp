5ea1bc17602cbd8aeaa126e12e7581c1fc521627
wektor1 <wektor1@gmail.com>
2018-07-01 11:29:15 +0200

    Added hello.hpp, modified hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "hello.cpp"
 
 int main()
 {
-    using namespace std;
-    cout<<"hello world"<<endl;
-    return 0;
+	using namespace std;
+	cout<<"hello world""<<endl;
+	cout<<"Hello Coders School"<<endl;
+	return 0;
 }
---
+++ hello.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+int i =10;
+
